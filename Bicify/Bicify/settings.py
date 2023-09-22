8b2564bec3d2bba8381d4a07bb3de2fe05c17f68
GloriaFlores03 <gloria.flores.ceb@immune.institute>
2023-09-22 21:34:10 +0200

    bbdd creada 1.0

--- Bicify/Bicify/settings.py
+++ Bicify/Bicify/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-b@(2hlie9igrl5=eho#x-)63l3)3*2^(^&&1dslumvhcav4_8m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "*"
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
     'Bicify.apps.MyAppConfig',
 ]
 
